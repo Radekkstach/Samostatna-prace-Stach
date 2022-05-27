056610a14dca68add9c2386f2fee356353f1cfe3
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2022-05-27 13:15:12 +0200

    Add files via upload

---
+++ samostatnapracemetodystach/samostatnapracemetodystach/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace samostatnapracemetodystach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(52, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Úkol 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(299, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Úkol 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.Maroon;
+            this.button3.Location = new System.Drawing.Point(555, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Úkol 3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkGray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ForeColor = System.Drawing.Color.Maroon;
+            this.button4.Location = new System.Drawing.Point(299, 458);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Vypnout";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(677, 505);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ samostatnapracemetodystach/samostatnapracemetodystach/Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace samostatnapracemetodystach
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        string picoviny = "0123456789qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM";
+        bool JeAlfanum(ref int malapismena, ref int velkapismena, ref int jineznaky)
+        {
+            malapismena = 0;
+            velkapismena = 0;
+            jineznaky = 0;
+            string veta = textBox1.Text;
+            veta = veta.Trim();
+            while(veta.Contains("  "))
+            {
+                veta = veta.Replace("  "," ");
+            }
+            foreach (char znak in veta)
+            {
+                if(picoviny.Contains(znak))
+                {
+                    if(znak >= 'a' && znak <= 'z')
+                    {
+                        malapismena++;
+                    }
+
+                    if (znak >= 'A' && znak <= 'Z')
+                    {
+                        velkapismena++;
+                    }
+
+
+                }
+                else
+                {
+                    if(znak > 32) jineznaky++;
+                    
+                }
+            }
+            if (jineznaky == 0) return true;
+            else return false;
+            
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int malapismena = 0;
+            int velkapismena = 0;
+            int jineznaky = 0;
+            if(JeAlfanum(ref malapismena,ref velkapismena, ref jineznaky) == true)
+            {
+                label2.Text = "Je Alfanumericky\n" + "Pocet malych pismen " + malapismena + "\nPocet velkych pismen " + velkapismena + "\nPocet jinych znaku " + jineznaky;
+            }
+            else
+            {
+                label2.Text = "Neni Alfanumericky\n" + "Pocet malych pismen " + malapismena + "\nPocet velkych pismen " + velkapismena + "\nPocet jinych znaku " + jineznaky;
+            }
+
+        }
+    }
+}
